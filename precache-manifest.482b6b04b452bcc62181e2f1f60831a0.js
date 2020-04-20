b100eaa9ddc323dab4b1bffa71a70dd2b209116a
abdelhakhamdouni <abdelhakhamdouni@sfr.fr>
2020-04-20 19:07:40 +0200

    Updates

---
+++ precache-manifest.482b6b04b452bcc62181e2f1f60831a0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e0ff59c75dffdf0ec4f6db4833d621",
+    "url": "/muslim-reminder/index.html"
+  },
+  {
+    "revision": "2aaaa80618f486a8e490",
+    "url": "/muslim-reminder/static/css/main.d40c9a53.chunk.css"
+  },
+  {
+    "revision": "7816a953e6a1681a9fdc",
+    "url": "/muslim-reminder/static/js/2.34a6a2d2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/muslim-reminder/static/js/2.34a6a2d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aaaa80618f486a8e490",
+    "url": "/muslim-reminder/static/js/main.4b628a41.chunk.js"
+  },
+  {
+    "revision": "0996c69088479f2f3563",
+    "url": "/muslim-reminder/static/js/runtime-main.9107e9ee.js"
+  },
+  {
+    "revision": "6da6297e43ee42fa89e5632ff48b968c",
+    "url": "/muslim-reminder/static/media/a7.6da6297e.mp3"
+  },
+  {
+    "revision": "eddb358d7ac8a458c85b80456ee79b5b",
+    "url": "/muslim-reminder/static/media/logo.eddb358d.jpg"
+  },
+  {
+    "revision": "a79b204e9c3055c77f0d81921bd881c2",
+    "url": "/muslim-reminder/static/media/me_quran-webfont.a79b204e.ttf"
+  }
+]);
